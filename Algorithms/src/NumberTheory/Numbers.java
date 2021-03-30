007ebf1a01a67a159fbfc4fae0ff5d6d33361626
Gana Sai Pravee <ganarko@yahoo.com>
2021-03-30 11:45:33 +0530

    nCr Formula Added

--- Algorithms/src/NumberTheory/Numbers.java
+++ Algorithms/src/NumberTheory/Numbers.java
@@ -216,4 +216,49 @@ public class Numbers {
 		matrixExpo(M, n-2);
 		return (M[0][0]+M[0][1]);
 	}
+
+	public static int FindNcR(int n, int r)
+    {
+ 
+        // p holds the value of n*(n-1)*(n-2)...,
+        // k holds the value of r*(r-1)...
+        int p = 1, k = 1;
+ 
+        // C(n, r) == C(n, n-r),
+        // choosing the smaller value
+        if (n - r < r) {
+            r = n - r;
+        }
+ 
+        if (r != 0) {
+            while (r > 0) {
+                p *= n;
+                k *= r;
+ 
+                // gcd of p, k
+                long m = gcd(p, k);
+ 
+                // dividing by gcd, to simplify
+                // product division by their gcd
+                // saves from the overflow
+                p /= m;
+                k /= m;
+ 
+                n--;
+                r--;
+            }
+ 
+            // k should be simplified to 1
+            // as C(n, r) is a natural number
+            // (denominator should be 1 ) .
+        }
+        else {
+            p = 1;
+        }
+ 
+        // if our approach is correct p = ans and k =1
+       // System.out.println(p);
+	   return p;
+    }
+
 }
